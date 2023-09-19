ccded9534a387303f9729c4f3991471d079d71ba
GassinDev <juangassintic@gmail.com>
2023-09-19 10:58:32 +0200

    avanzamos

---
+++ DWES/holamundo.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=º, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<p>Hola mundo</p>';
+        $coches = 3;
+        $resul = 0;
+        //comentario
+        echo $coches;
+
+        /*
+        Mas de una linea
+        */
+        echo '<p></p>';
+        echo $COCHES = 2;
+
+        for($coches; $coches < 9; $coches++){
+
+            $resul = $resul + 1;
+            echo $resul;
+        }
+    ?>
+</body>
+</html>
