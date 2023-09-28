622a3fa4bd73403aca8f0c1a3f36adc1a3396348
JuanGassinDAW <juangaming97@gmail.com>
2023-09-29 01:35:41 +0200

    poco

--- DWEC/T2/Ejercicios/ListaEjerciciosJSBásico_3/Ejercicios3.js
+++ DWEC/T2/Ejercicios/ListaEjerciciosJSBásico_3/Ejercicios3.js
@@ -1,6 +1,20 @@
+function calcularTiempo(){
+
+    let fecha = new Date();
+
+    let mesActual = fecha.getMonth()+1;
+    let diaActual = fecha.getDate();
+    let horasActuales = fecha.getHours();
+
+    let mesesRestantes = 12 - mesActual;
+    let diasRestantes = 31 - diaActual;
+
+    alert(`Quedan ${diasRestantes} días, ${mesesRestantes} meses. ${horaActuales}`)
+}
+
 function mostrar(){
 
 let pi = Math.PI;
-
+1695944041477
     alert((pi).toFixed(4));
 }
