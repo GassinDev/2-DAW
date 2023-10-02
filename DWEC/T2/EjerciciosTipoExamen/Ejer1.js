cdbe1dbb2b308876cba1e6a571061ece07c404b2
GassinDev <juangassintic@gmail.com>
2023-10-02 14:29:34 +0200

    ole

--- DWEC/T2/EjerciciosTipoExamen/Ejer1.js
+++ DWEC/T2/EjerciciosTipoExamen/Ejer1.js
@@ -1,18 +1,22 @@
+let segundos = 5;
+
 function crearJuego(){
-    document.write("<h1>Consigue clickar sobre todos los números primos</h1>")
+    document.write("<h1>Consigue clickar sobre todos los números primos</h1>");
+    document.write("<span id='tiempo' style='font-size:20px'></span><br>");
     for(let i = 1; i <= 50;i++){
         if(i < 10){
-            document.write(`<input type="button" id='${i}' value='0${i}' onclick='comprobarPrimo(${i})'></input>`);
+            document.write(`<input type="button" style='min-width:32px'id='${i}' value='0${i}' onclick='comprobarPrimo(${i})'></input>`);
         }else if(i % 10 === 0){
-            document.write(`<input type="button" id='${i}' value='${i}' onclick='comprobarPrimo(${i})'></input><br>`);
+            document.write(`<input type="button" style='min-width:32px' id='${i}' value='${i}' onclick='comprobarPrimo(${i})'></input><br>`);
         }else{
-            document.write(`<input type="button" id='${i}' value='${i}' onclick='comprobarPrimo(${i})'></input>`);
+            document.write(`<input type="button" style='min-width:32px' id='${i}' value='${i}' onclick='comprobarPrimo(${i})'></input>`);
         }
     }
+    setInterval(temporizador,1000);
 }
 
 function comprobarPrimo(numero){
-    document.getElementById("numero").style.color = "red";
+
     let esPrimo = true;
 
     if (numero <= 1) {
@@ -26,6 +30,18 @@ function comprobarPrimo(numero){
     }
 
     if(esPrimo){
-        
+        document.getElementById(numero).value = "__";
+    }else{
+        document.getElementById(numero).style.backgroundColor = "red";
+    }
+}
+
+
+function temporizador(){
+    segundos--;
+    document.getElementById("tiempo").innerHTML = `Temporizador : ${segundos} segundos`;
+    if(segundos === -1){
+        alert("Lo siento, se le ha acabado el tiempo.");
+        window.location.reload();
     }
 }
