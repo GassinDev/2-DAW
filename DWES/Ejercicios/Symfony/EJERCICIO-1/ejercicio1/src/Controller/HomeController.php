b4faae43583277c48516e15f04134b0a5d2796cf
GassinDev <juangassintic@gmail.com>
2024-01-15 09:44:39 +0100

    avances

---
+++ DWES/Ejercicios/Symfony/EJERCICIO-1/ejercicio1/src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    #[Route('/home')]
+    public function index(): Response
+    {
+        $message = '¡Bienvenido!';
+
+        return new Response($message);
+    }
+}
+
+?>
---
+++ DWES/Ejercicios/Symfony/EJERCICIO-1/ejercicio1/src/Controller/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/about')]
+    public function about(): Response
+    {
+
+        return $this->render('/about.html.twig', [
+            'title' => 'Sobre mi pagina web',
+        ]);
+        
+    }
+
+    #[Route('/contact')]
+    public function contact(): Response
+    {
+        return $this->render('/contact.html.twig', [
+            'title' => 'Contact: ',
+        ]);
+    }
+}
+
+?>
