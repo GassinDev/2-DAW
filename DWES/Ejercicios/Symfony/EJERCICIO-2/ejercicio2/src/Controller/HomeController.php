3c121bad8c9ec009350ba614ea016d8e5df3f796
GassinDev <juangassintic@gmail.com>
2024-01-24 14:23:40 +0100

    avances

--- DWES/Ejercicios/Symfony/EJERCICIO-2/ejercicio2/src/Controller/HomeController.php
+++ DWES/Ejercicios/Symfony/EJERCICIO-2/ejercicio2/src/Controller/HomeController.php
@@ -7,30 +7,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ProductoType;
+use Doctrine\DBAL\Types\FloatType;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'title' => 'Mi tienda',
         ]);
     }
 
-    #[Route('/saveProducto')]
-    public function new (EntityManagerInterface $entityManager) : Response{
-        $s = new Producto();
-        $s->setNombre("Leche");
-        $s->setDescripcion("Bebida 100% de Vaca");
-        $s->setPrecio(1.50);
-        $s->setCantidad(4);
+    #[Route('/formProdu', name: 'formularioProducto')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $producto = new Producto();
+        $form = $this->createForm(ProductoType::class, $producto);
+        $form->handleRequest($request);
 
-        $entityManager->persist($s);
-        $entityManager->flush();
-        
-        return new Response(sprintf("Introducido el producto con éxito."));
-        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($producto);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('save');
+        }
+
+        return $this->render('form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+        //Request $request, 
+    //     $producto = new Producto();
+    //     // $s->setNombre("Leche");
+    //     // $s->setDescripcion("Bebida 100% de Vaca");
+    //     // $s->setPrecio(1.50);
+    //     // $s->setCantidad(4);
+
+    //     $form = $this->createFormBuilder($producto)
+    // ->add('nombre', TextType::class)
+    // ->add('id', NumberType::class)
+    // ->add('descripcion', TextType::class)
+    // ->add('precio', FloatType::class)
+    // ->add('cantidad', NumberType::class, ['label' => 'Crear Producto'])
+    // ->getForm();    
     }
 
     #[Route('/getProducto')]
@@ -39,10 +63,30 @@ class HomeController extends AbstractController
         $productoRespository = $entityManager->getRepository(Producto::class);
         $producto = $productoRespository->findAll();
         dd($producto);
-    
+
         return new Response(sprintf(
             'CONSULTA CON ÉXITO',
         ));
-    
+    }
+
+    #[Route('/saveProducto', name: 'save')]
+    public function guardado(): Response
+    { 
+
+        return $this->render('save.html.twig', [
+            'message' => "Producto añadido con éxito",
+        ]);
+    }
+
+    #[Route("/listarProductos", name: 'listado')]
+    public function listar(EntityManagerInterface $entityManager): Response
+    {
+
+        $productoRepository = $entityManager->getRepository(Producto::class);
+        $producto = $productoRepository->findAll();
+        
+        return $this->render('listar.html.twig', [
+            'arrayproducto' => $producto,
+        ]);
     }
 }
