5ea5807dcd627ae3f8c77af9861a148d9dd44784
JuanGassinDAW <juangaming97@gmail.com>
2024-03-06 23:05:02 +0100

    AVANCES

--- DWEC/2-TRIM/React/Proyecto React/2XKO_FANART/src/components/ListaPersonajes.jsx
+++ DWEC/2-TRIM/React/Proyecto React/2XKO_FANART/src/components/ListaPersonajes.jsx
@@ -1,66 +1,44 @@
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, ProgressBar } from 'react-bootstrap';
 import personajesDatos from '../data/personajes.json';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 
-// Componente para mostrar los personajes del json
 const ListaPersonajes = () => {
+    const [personajeSeleccionado, setPersonajeSeleccionado] = useState(null);
+    const [filtroEstilo, setFiltroEstilo] = useState('');
+    const [filtroNombre, setFiltroNombre] = useState('');
 
-    // Declaración de estados utilizando useState
-    const [personajeSeleccionado, setPersonajeSeleccionado] = useState(null); // Estado para el personaje seleccionado
-    const [filtroEstilo, setFiltroEstilo] = useState(''); // Estado para el filtrado por estilos
-    const [filtroNombre, setFiltroNombre] = useState(''); // Estado para el filtrado por nombres
-
-    // Manejador de evento para seleccionar un personaje
     const handleSeleccionarPersonaje = (index) => {
         if (personajeSeleccionado === index) {
-            // Desselecciona el personaje si ya estaba seleccionado
             setPersonajeSeleccionado(null);
         } else {
-            // Selecciona el personaje
-            setPersonajeSeleccionado(index); 
+            setPersonajeSeleccionado(index);
         }
     };
 
-    // Manejador de evento para filtrar por estilo
     const handleFiltrarPorEstilo = (estilo) => {
-        // Establece el filtro de estilo o lo borra si ya estaba aplicado
-        setFiltroEstilo(estilo === filtroEstilo ? '' : estilo); 
+        setFiltroEstilo(estilo === filtroEstilo ? '' : estilo);
     };
 
-    // Manejador de evento para filtrar por nombre
     const handleFiltrarPorNombre = (nombre) => {
-        // Establece el filtro de nombre
-        setFiltroNombre(nombre); 
+        setFiltroNombre(nombre);
     };
 
-    // Manejador de evento para limpiar todos los filtros para el boton de Mostrar Todos
-    const handleLimpiarFiltros = () => {
-        // Borra el filtro de estilo
-        setFiltroEstilo('');
-        // Borra el filtro de nombre
-        setFiltroNombre(''); 
-    };
 
-    // Filtra los personajes con cualquier filtro aplicado.
     const personajesFiltrados = personajesDatos.personajes.filter(personaje => {
-        return (filtroEstilo === '' || personaje.estilo === filtroEstilo) && // Filtra por estilo si está definido
-            (filtroNombre === '' || personaje.nombre.toLowerCase().includes(filtroNombre.toLowerCase())); // Filtra por nombre si está definido
+        return (filtroEstilo === '' || personaje.estilo === filtroEstilo) &&
+            (filtroNombre === '' || personaje.nombre.toLowerCase().includes(filtroNombre.toLowerCase()));
     });
 
     return (
         <div style={{ marginRight: '15px', marginLeft: '15px' }}>
             <h2>Personajes de 2XKO</h2>
-            {/* Botones para filtrar por estilo */}
             <div className='mb-5 text-center'>
-                <button className={filtroEstilo === '' ? 'elegido' : 'btn-customPerso'} onClick={handleLimpiarFiltros}>Mostrar Todos</button>
+                <button className={filtroEstilo === '' ? 'elegido' : 'btn-customPerso'} onClick={() => handleFiltrarPorEstilo('')}>Mostrar Todos</button>
                 <button className={filtroEstilo === 'Tanque' ? 'elegido' : 'btn-customPerso'} onClick={() => handleFiltrarPorEstilo('Tanque')}>Tanques</button>
                 <button className={filtroEstilo === 'Tirador' ? 'elegido' : 'btn-customPerso'} onClick={() => handleFiltrarPorEstilo('Tirador')}>Tiradores</button>
                 <button className={filtroEstilo === 'Luchador' ? 'elegido' : 'btn-customPerso'} onClick={() => handleFiltrarPorEstilo('Luchador')}>Luchadores</button>
-                {/* Campo de búsqueda para filtrar por nombre */}
                 <input type="text" className='buscador-custom' placeholder="Buscar" onChange={(e) => handleFiltrarPorNombre(e.target.value)} />
             </div>
-            {/* Contenedor para mostrar los personajes filtrados */}
             <div className='distribucion'>
                 {personajesFiltrados.map((personaje, index) => (
                     <Card
@@ -68,10 +46,8 @@ const ListaPersonajes = () => {
                         className={`designCard ${personajeSeleccionado === index ? 'seleccionado' : ''}`}
                         onClick={() => handleSeleccionarPersonaje(index)}
                     >
-                        {/* Contenido de cada tarjeta de personaje */}
                         <div className="position-relative">
                             <Card.Img variant="top" src={personaje.foto} alt={personaje.nombre} />
-                            {/* Overlay que muestra las estadísticas del personaje al seleccionarlo */}
                             {personajeSeleccionado === index && (
                                 <div className="overlay">
                                     <div>
